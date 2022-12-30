bfaac0db8f387aa29627c014c680df9dbd1df5d8
Real-Artisan <iwenyadaniel12@gmail.com>
2022-12-30 16:00:22 +0100

    close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=3d4442309a4b471d82a800a9fd608628';
   title = 'find_me';
+  result: any;
+  see_more = false;
+  constructor(private http: HttpClient){
+    this.getLocationData();
+  }
+
+  getLocationData(){
+    this.http.get(this.url).subscribe((response: any) =>{
+      this.result = response;
+      console.log(response)
+    })
+  }
 }
